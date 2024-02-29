030bf36d7c224b8479128dbcaa5caf50c5810cc5
ashish <1508ramashish@gmail.com>
2024-02-29 16:25:17 +0530

    file add

---
+++ Deployment/Intr.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Intr = () => {
+  return (
+    <div>
+       1. for Deployment  we used (render.com )and (netlify)
+      2.frontend on netlify  
+      3.Backend on reder.com
+    </div>
+  )
+}
+
+export default Intr
---
+++ Important/Tool.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Tool = () => {
+  return (
+    <div>
+   ICON::::   Font owsome is website which provide icon (its has also package to install in react app)
+   SERVER BUILD::: 
+     1. make package.json file(npm init ) write its decription in terminal pop up
+     2. make node_modules folder(npm install nodemon --save-dev) 
+     3.install various package to used it using (npm i |package_name|) see these package is install sucessfully in package.json --- dependecy
+     4: make index.js file for entrypoint,make db.js for database connection function,make modal.js for schema define(colltion), make router.js for  route and its funtionallity 
+     5. install core package to used route
+    SERVER CONNECT WITH FRONT END:::::
+       1. add fetch(url/route which we want to connet) in any event listerner function
+    RUNING INSTRUCTION:::'
+    1. backend run with (nodemon index.js )   frontend run  (npm run start)
+    NODEMON :: its is tool which restart the server whenever change is occur 
+    DATABASE TIME::::greenwitch mean time 
+    DEPLOYMENT SITE::   versal  and netlify ,render
+    
+    </div>
+  )
+}
+
+export default Tool
